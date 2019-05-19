83934740ab9144d6bbfe5dab7409beb239e6bad3
nguyentamdat <nguyentamdat@gmail.com>
2019-05-19 21:51:49 +0700

    new branhc

--- src/components/TransactionScreen/TransactionScreen.js
+++ src/components/TransactionScreen/TransactionScreen.js
@@ -149,6 +149,49 @@ class TransactionScreen extends Component {
                 </ListItem>
               );
             })}
+            {/* <List
+              rightOpenValue={-75}
+              renderRow={x => (
+                <ListItem key={x.key} noIndent>
+                  <View style={styles.iconListItem}>
+                    <Button transparent style={{ alignSelf: "center" }}>
+                      <Icon
+                        active
+                        type={x.purpose.iconType}
+                        name={x.purpose.iconName}
+                      />
+                    </Button>
+                  </View>
+                  <Body
+                    style={{
+                      flexDirection: "column",
+                      flex: 1,
+                      alignContent: "flex-start"
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: x.purpose.isRevenue ? "#00cc44" : "#e60000",
+                        fontSize: 24
+                      }}
+                    >
+                      {x.amount}
+                    </Text>
+                    <Text>{x.purpose.name}</Text>
+                  </Body>
+                  <Right style={{ flex: 1 }}>
+                    <Text>{date.toLocaleDateString()}</Text>
+                    <Text>{date.toLocaleTimeString()}</Text>
+                  </Right>
+                </ListItem>
+              )}
+              dataSource={this.state.listTrans}
+              renderRightHiddenRow={x => (
+                <Button full danger onPress={x => {}}>
+                  <Icon active name="trash" />
+                </Button>
+              )}
+            /> */}
           </Content>
         </Container>
       );
